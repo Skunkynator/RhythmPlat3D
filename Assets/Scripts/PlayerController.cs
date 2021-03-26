f366a5e2d7b49036b1cd6a05d61f4ef395900d9c
Skunkynator <43096109+Skunkynator@users.noreply.github.com>
2021-03-26 15:28:47 +0100

    Player Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,16 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     float mouseSensitivity = 1;
+    [SerializeField]
+    float speed = 10;
     Camera playerCamera;
+    Rigidbody playerRigid;
+    Vector3 forwards = Vector3.forward;
+    Vector3 up = Vector3.up;
+    Vector3 left = Vector3.left;
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         playerCamera = GetComponentInChildren<Camera>();
+        playerRigid = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -20,13 +28,24 @@ public class PlayerController : MonoBehaviour
         float mouseY = Input.GetAxisRaw("Mouse Y");
         float mouseX = Input.GetAxisRaw("Mouse X");
         Vector3 camEuler = playerCamera.transform.eulerAngles;
+        Vector3 dirControlls = Vector3.zero;
 
         mouseY *= Time.deltaTime * mouseSensitivity;
         mouseX *= Time.deltaTime * mouseSensitivity;
         //Debug.Log(euler.x);
         camEuler.x = Mathf.Clamp(fixDegree(camEuler.x - mouseY), -87, 87);
         camEuler.y += mouseX;
+        forwards.x = Mathf.Sin(camEuler.y * Mathf.Deg2Rad);
+        forwards.z = Mathf.Cos(camEuler.y * Mathf.Deg2Rad);
+        left = Vector3.Cross(forwards, up);
 
+        dirControlls += Input.GetAxis("Vertical")   * forwards;
+        dirControlls -= Input.GetAxis("Horizontal") * left;
+        dirControlls = dirControlls.normalized * Mathf.Clamp01(dirControlls.magnitude);
+        dirControlls *= Time.deltaTime * speed;
+        playerRigid.MovePosition(playerRigid.position + dirControlls);
+
+        
         playerCamera.transform.eulerAngles = camEuler;
     }
 
@@ -43,6 +62,13 @@ public class PlayerController : MonoBehaviour
             GameObject child = new GameObject("Player Camera");
             child.AddComponent<Camera>();
             child.transform.SetParent(transform);
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
         }
     }
 }
